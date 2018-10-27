8d08bf588a1cae9d13d6561f31afc66f27ebb1f3
Kostas Agnantis <kagnadis@gmail.com>
2018-10-27 21:41:39 +0000

    Bumps megaparsec version from 6.5 to 7.0. WIP: Adds new parsers (ontology and annotation)

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, iri, megaparsec, parser-combinators, stdenv, devDeps ? [ ] }:
+{ mkDerivation, base, iri, megaparsec_7_0_0, parser-combinators, stdenv, devDeps ? [ ] }:
 
 mkDerivation {
   pname = "owl-parser";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base iri megaparsec parser-combinators ] ++ devDeps;
+  libraryHaskellDepends = [ base iri megaparsec_7_0_0 parser-combinators ] ++ devDeps;
   homepage = "github.com/agnantis/owl-parser";
   description = "OWL2 parser";
   license = stdenv.lib.licenses.bsd3;
--- src/OwlParser.hs
+++ src/OwlParser.hs
@@ -1,40 +1,84 @@
 module OwlParser where
 
--- import Data.Text hiding (empty)
--- import Data.Char (digitToInt)
+import           Prelude                           hiding ( exponent )
+import           Data.Maybe                               ( fromMaybe )
+import           Data.Void
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer    as L
 
-import Prelude hiding (exponent)
-import Data.Maybe (fromMaybe)
-import Data.Void
-import Text.Megaparsec
-import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
-
+---- TYPES ----
 type Parser = Parsec Void String
+type LangTag = String
+type IRI = String
+type Prefix = String
+
+data TypedLiteral = TypedL String String deriving Show
+data FloatPoint = Float Double (Maybe Exponent)
+
+newtype Exponent = Exponent Integer
+newtype DecimalLiteral = DecimalL Double deriving (Show)
+newtype IntegerLiteral = IntegerL Integer deriving (Show)
+
+instance Show FloatPoint where
+  show (Float n me) = concat [show n, maybe "" show me]
+instance Show Exponent where
+  show (Exponent i) = concat ["e", if i > 0 then "+" else "", show i]
 
 -- | Parses white space and line comments
+--
+-- parsec (sc >> satisfy (const true)) "    some indented text"
+-- "some individual text"
 sc :: Parser ()
-sc = L.space space1 lineComment empty
-  where lineComment = L.skipLineComment "#"
+sc = L.space space1 (L.skipLineComment "#") empty
+
 
 -- | Parses the actual lexeme and then any remaining space
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
 
 iriParens :: Parser a -> Parser a
-iriParens = between (symbol "<") (symbol ">")
+iriParens = between (char '<') (char '>')
 
 parens :: Parser a -> Parser a
-parens = between (symbol "(") (symbol ")")
+parens = between (char '(') (char ')')
 
-keywords :: [String]
-keywords = ["integer", "decimal", "float", "string", "Datatype", "Class", "ObjectProperty", "DataProperty", "AnnotationProperty", "NamedInvividual"]
+-- | Reserved keywords
+allKeywords :: [String]
+allKeywords =
+  concat [datatypeKeywords, entityKeywords, ontologyKeywords, propertyKeywords]
+
+datatypeKeywords :: [String]
+datatypeKeywords = ["integer", "decimal", "float", "string"]
+
+entityKeywords :: [String]
+entityKeywords =
+  [ "Datatype"
+  , "Class"
+  , "ObjectProperty"
+  , "DataProperty"
+  , "AnnotationProperty"
+  , "NamedInvividual"
+  ]
 
 ontologyKeywords :: [String]
-ontologyKeywords = ["Annotations:", "Prefix:", "Ontology:", "Import:"]
+ontologyKeywords = ["Annotations", "Prefix", "Ontology", "Import"]
 
 propertyKeywords :: [String]
-propertyKeywords = ["inverse", "or", "and", "not", "length", "minLength", "maxLength", "pattern", "<=", "<", ">=", ">"]
+propertyKeywords =
+  [ "inverse"
+  , "or"
+  , "and"
+  , "not"
+  , "length"
+  , "minLength"
+  , "maxLength"
+  , "pattern"
+--  , "<="
+--  , "<"
+--  , ">="
+--  , ">"
+  ]
 
 -- | Parses the symbol and then any remaining space
 --
@@ -114,10 +158,6 @@ sign = do
 fullIRI :: Parser IRI
 fullIRI = iriParens $ takeWhileP Nothing (/= '>')
 
--- | Reserved keywords
-rws :: [String]
-rws = ["Ontology", "ObjectProperty", "Class", "Prefix"]
-
 -- | It parses arbitrary alpharithmetics provived that it does not belong to
 -- the list of reserved keywords
 --
@@ -140,25 +180,16 @@ rws = ["Ontology", "ObjectProperty", "Class", "Prefix"]
 -- keyword "Ontology" cannot be an identifier
 identifier :: Parser String
 identifier = (lexeme . try) (p >>= check)
-  where
-    p       = (:) <$> letterChar <*> many (alphaNumChar <|> char '_')
-    check x = if x `elem` rws
-                then fail $ concat ["keyword ", show x, " cannot be an identifier"]
-                else return x
+ where
+  p = (:) <$> letterChar <*> many (alphaNumChar <|> char '_')
+  check x = if x `elem` allKeywords
+    then fail $ concat ["keyword ", show x, " cannot be an identifier"]
+    else return x
 
-type IRI = String
-type Prefix = String
--- | It parses prefix names. Format: 'Prefix: <name>: <IRI>
--- >>> parseTest prefixName "Prefix: owl: <http://www.w3.org/2002/07/owl#>"
--- ("owl","http://www.w3.org/2002/07/owl#")
+-- | It parses prefix names
 --
-prefixName :: Parser (String, IRI)
-prefixName = do
-  string "Prefix:"
-  sc
-  prefix <- identifier <* symbol ":"
-  iri <- fullIRI
-  return (prefix, iri)
+prefixName :: Parser String
+prefixName = identifier
 
 -- | It parses abbreviated IRIs. Format: 'prefix:term'
 -- >>> parseTest abbreviatedIRI "owl:user1"
@@ -177,8 +208,7 @@ simpleIRI = identifier
 -- | It parses any of the three different formats of IRIs
 iri :: Parser String
 iri = fullIRI <|> (concatAbbrIRI <$> abbreviatedIRI) <|> simpleIRI
-  where
-    concatAbbrIRI (x, y) = concat [x, ":", y]
+  where concatAbbrIRI (x, y) = concat [x, ":", y]
 
 -- | It parses class IRIs
 classIRI :: Parser IRI
@@ -193,7 +223,12 @@ classIRI = iri
 -- "integer"
 --
 dataType :: Parser String
-dataType = try dataTypeIRI <|> symbol "integer" <|> symbol "decimal" <|> symbol "float" <|> symbol "string"
+dataType =
+  try dataTypeIRI
+    <|> symbol "integer"
+    <|> symbol "decimal"
+    <|> symbol "float"
+    <|> symbol "string"
 
 dataTypeIRI :: Parser String
 dataTypeIRI = iri
@@ -229,35 +264,57 @@ individualIRI = iri
 -- expecting "_:"
 --
 nodeID :: Parser String
-nodeID = (++) <$> symbol "_:" <*> identifier 
+nodeID = (++) <$> symbol "_:" <*> identifier
 
 literal :: Parser String
-literal = show <$> typedLiteral <|> stringLiteralNoLanguage <|> stringLiteralWithLanguage <|> show <$> integerLiteral <|> show <$> decimalLiteral <|> show <$> floatingPointLiteral
+literal =
+  show
+    <$> typedLiteral
+    <|> stringLiteralNoLanguage
+    <|> fmap (\(v, l) -> concat [v, "@", l]) stringLiteralWithLanguage
+    <|> (show <$> integerLiteral)
+    <|> (show <$> decimalLiteral)
+    <|> (show <$> floatingPointLiteral)
 
-data TypedLiteral = TypedL String String deriving Show
-
---instance Show TypedLiteral where
---  show (TypedL lxc dt) = concat ["\"", lxc, "\"^^", dt]
- 
--- | Parses a typed literal
+-- | It parses a typed literal
 --
 -- >>> parseTest typedLiteral "\"32\"^^integer"
 -- TypedL "32" "integer"
 typedLiteral :: Parser TypedLiteral
-typedLiteral = TypedL <$> lexicalValue <*> (symbol "^^" >>  dataType)
+typedLiteral = TypedL <$> lexicalValue <*> (symbol "^^" >> dataType)
 
-
+-- | It parses a string value with no language tag
+--
+-- >>> parseTest stringLiteralNoLanguage "\"hello there\""
+-- "hello there"
+--
 stringLiteralNoLanguage :: Parser String
 stringLiteralNoLanguage = quotedString
 
-stringLiteralWithLanguage :: Parser String
-stringLiteralWithLanguage = do
-   qString <- quotedString
-   lTag <- languageTag
-   return "<stringLiteralWithLanguage>"
 
+-- | It parses a string value with language tag
+--
+-- >>> parseTest stringLiteralWithLanguage "\"hello there\"@en"
+-- ("hello there","en")
+--
+stringLiteralWithLanguage :: Parser (String, LangTag)
+stringLiteralWithLanguage = do
+  qString <- quotedString
+  lTag    <- languageTag
+  return (qString, lTag)
+
+-- | It parse a language tag
+--
+-- parseTest languageTag "@en"
+-- "en"
+-- parseTest languageTag "en"
+-- ...
+-- unexpected 'e'
+-- expecting '@'
+--
+-- TODO: check for valid lang tags
 languageTag :: Parser String
-languageTag = symbol "@" >> return "<languageTag>" -- (U+40) followed a nonempty sequence of characters matching the langtag production from [BCP 47]
+languageTag = char '@' >> identifier -- (U+40) followed a nonempty sequence of characters matching the langtag production from [BCP 47]
 
 lexicalValue :: Parser String
 lexicalValue = quotedString
@@ -270,28 +327,18 @@ lexicalValue = quotedString
 -- "this is \\\"test \\\" message"
 quotedString :: Parser String
 quotedString = do
-    char '\"'
-    strings <- many chars
-    char '\"'
-    return $ concat strings
-  where
-    chars = fmap return nonEscape <|> escape
-    nonEscape = noneOf "\\\"\0\n\r\v\t\b\f" -- all the characters that can be escaped
-    escape = do
-      d <- char '\\'
-      c <- oneOf "\\\"0nrvtbf"
-      return [d,c]
+  char '\"'
+  strings <- many chars
+  char '\"'
+  return $ concat strings
+ where
+  chars     = fmap return nonEscape <|> escape
+  nonEscape = noneOf "\\\"\0\n\r\v\t\b\f" -- all the characters that can be escaped
+  escape    = do
+    d <- char '\\'
+    c <- oneOf "\\\"0nrvtbf"
+    return [d, c]
 
-
-data FloatPoint = Float Double (Maybe Exponent)
-newtype Exponent = Exponent Integer
-newtype DecimalLiteral = DecimalL Double deriving (Show)
-newtype IntegerLiteral = IntegerL Integer deriving (Show)
-
-instance Show FloatPoint where
-  show (Float n me) = concat [show n, maybe "" show me]
-instance Show Exponent where
-  show (Exponent i) = concat ["e", if i > 0 then "+" else "", show i]
 
 -- | It parses folating point numbers.
 -- Valid formats:
@@ -314,26 +361,26 @@ instance Show Exponent where
 -- -12.3e-10
 floatingPointLiteral :: Parser FloatPoint
 floatingPointLiteral = do
-  sgn <- sign
+  sgn  <- sign
   dgts <- dig1 <|> dig2
   mExp <- optional exponent
   symbol "f" <|> symbol "F"
   return $ Float (read (sgn ++ dgts)) mExp
  where
   dig1 = do
-    dg' <- digits
+    dg'  <- digits
     mDec <- optional $ do
-         symbol "."
-         dg <- digits
-         return $ '.':dg
+      symbol "."
+      dg <- digits
+      return $ '.' : dg
     let dc = fromMaybe "" mDec
     return $ dg' ++ dc
   dig2 = do
-    _ <- symbol "."
+    _    <- symbol "."
     dgts <- digits
     return $ "0." ++ dgts
-    
-  
+
+
 -- | It parses an exponent
 --
 -- >>> parseTest exponent "e10"
@@ -351,7 +398,7 @@ floatingPointLiteral = do
 exponent :: Parser Exponent
 exponent = do
   symb <- symbol "e" <|> symbol "E"
-  ms <- sign
+  ms   <- sign
   dgts <- digits
   return . Exponent . read . concat $ [ms, dgts]
 
@@ -366,9 +413,9 @@ exponent = do
 decimalLiteral :: Parser DecimalLiteral
 decimalLiteral = do
   mSign <- sign
-  dig1 <- digits 
-  dig2 <- symbol "." >> digits
-  return . DecimalL . read . concat $ [mSign, dig1, ".", dig2] 
+  dig1  <- digits
+  dig2  <- symbol "." >> digits
+  return . DecimalL . read . concat $ [mSign, dig1, ".", dig2]
 
 -- | It parser integer values
 --
@@ -381,22 +428,103 @@ decimalLiteral = do
 integerLiteral :: Parser IntegerLiteral
 integerLiteral = do
   mSign <- sign
-  digs <- digits
+  digs  <- digits
   return . IntegerL . read . concat $ [mSign, digs]
 
 entity :: Parser String
 entity = choice $ fmap (uncurry classParser) alts
-  where classParser :: String -> Parser String -> Parser String
-        classParser s p = do
-          smb <- symbol s
-          name <- p
-          return . concat $ ["<", s, ">"]
-        alts :: [(String, Parser String)] 
-        alts = [ ("Datatype", dataType)
-               , ("Class", classIRI)
-               , ("ObjectProperty", objectPropertyIRI)
-               , ("DataProperty",dataPropertyIRI)
-               , ("AnnotationProperty", annotationPropertyIRI)
-               , ("NamedIndividual", individualIRI)
-               ]
+ where
+  classParser :: String -> Parser String -> Parser String
+  classParser s p = do
+    smb  <- symbol s
+    name <- p
+    return . concat $ ["<", s, ">"]
+  alts :: [(String, Parser String)]
+  alts =
+    [ ("Datatype"          , dataType)
+    , ("Class"             , classIRI)
+    , ("ObjectProperty"    , objectPropertyIRI)
+    , ("DataProperty"      , dataPropertyIRI)
+    , ("AnnotationProperty", annotationPropertyIRI)
+    , ("NamedIndividual"   , individualIRI)
+    ]
+
+
+------------------------------
+-- Ontology and Annotations --
+------------------------------
+
+annotations :: Parser [String]
+annotations = symbol "Annotations:" >> undefined
+
+annotation :: Parser (String, String)
+annotation = (,) <$> annotationPropertyIRI <*> annotationTarget
+
+annotationTarget :: Parser String
+annotationTarget = nodeID <|> iri <|> literal
+
+ontologyDocument :: Parser String
+ontologyDocument = many prefixDeclaration >> ontology -- TODO: prefix is being ignored
+
+-- | It parses prefix names. Format: 'Prefix: <name>: <IRI>
+-- >>> parseTest prefixDeclaration "Prefix: owl: <http://www.w3.org/2002/07/owl#>"
+-- ("owl","http://www.w3.org/2002/07/owl#")
+--
+prefixDeclaration :: Parser (String, IRI)
+prefixDeclaration =
+  symbol "Prefix:" >> (,) <$> (prefixName <* symbol ":") <*> fullIRI
+
+ontology :: Parser String
+ontology = do
+  symbol "Ontology:"
+  ontoIRI <- optional $ (,) <$> ontologyIRI <*> optional versionIRI -- Maybe (iri, Maybe iri)
+  imports <- many importStmt
+  annots  <- many annotations
+  frames  <- many frame
+  return "TBA: <ontology>"
+
+ontologyIRI :: Parser IRI
+ontologyIRI = iri
+
+versionIRI :: Parser IRI
+versionIRI = iri
+
+importStmt :: Parser IRI
+importStmt = symbol "Import:" >> iri
+
+frame :: Parser String
+frame = undefined
+
+-- | It parses non empty lists
+--
+-- >>> parseTest (nonEmptyList languageTag) "@en, @el, @test"
+-- ["en","el","test"]
+--
+-- >>> parseTest (nonEmptyList languageTag) ""
+-- ...
+-- unexpected end of input
+-- expecting '@'
+--
+nonEmptyList :: Parser p -> Parser [p]
+nonEmptyList p = (:) <$> p <*> many (symbol "," >> p)
+
+-- | It parses lists with at least two elements
+--
+-- >>> parseTest (listOfAtLeast2 languageTag) "@en, @el, @test"
+-- ["en","el","test"]
+--
+-- >>> parseTest (listOfAtLeast2 languageTag) "@en"
+-- ...
+-- unexpected end of input
+-- ...
+--
+listOfAtLeast2 :: Parser p -> Parser [p]
+listOfAtLeast2 p = (:) <$> p <*> some (symbol "," >> p)
+
+-- | It parses non empty annotated lists
+--
+annotatedList :: Parser p -> Parser [([String], p)]
+annotatedList p =
+  let annotationList = (,) <$> fmap (fromMaybe []) (optional annotations) <*> p
+  in  nonEmptyList annotationList
 
